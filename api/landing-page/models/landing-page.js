4bca01dd28022fe0983f43a1691e0db5867a5f08
Felipe Jung <felipemattoseu@gmail.com>
2020-07-23 23:12:38 -0400

    Add netlifly webhook

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -4,5 +4,17 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
+const axios = require("axios")
+const netliflyWebook = strapi.config.get("custom.netliflyWebhook")
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+      async afterCreate(_, __){
+        netliflyWebook && axios.post(netliflyWebook)
+      },
+
+      async afterUpdate(_, __, ___){
+        netliflyWebook && axios.post(netliflyWebook)
+      }
+    },
+};
---
+++ config/env/production/custom.js
@@ -0,0 +1,3 @@
+module.exports = ({ env }) => ({
+  netliflyWebhook: env('NETLIFLY_WEBHOOK')
+})
